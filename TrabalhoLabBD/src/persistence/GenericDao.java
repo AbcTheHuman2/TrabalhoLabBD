fa1366e52d3f2e23b2e7338d3a83359a7909fad9
AbcTheHuman2 <athehuman@gmail.com>
2018-04-11 12:45:26 -0300

    Pacotes adicionados, além da classe GenericDao

---
+++ TrabalhoLabBD/src/persistence/GenericDao.java
@@ -0,0 +1,26 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GenericDao {
+
+private Connection c;
+
+public Connection getConnection() throws ClassNotFoundException, SQLException {
+
+String hostName = "localhost";
+String dbName ="trabalholabbd";
+String user ="login";
+String senha ="senha";
+
+Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+
+c = DriverManager.getConnection(String.format("jdbc:jtds:sqlserver://%s:1433;database=%s;user=%s;password=%s;", hostName, dbName, user, senha));
+
+return c;
+}
+
+}
